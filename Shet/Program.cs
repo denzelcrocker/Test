e8d1e7b9da0da2c3d202db8e25932e42a26c3c08
denzelcrocker <zelentsov36@gmail.com>
2022-07-03 12:15:56 +0300

    itog

--- Shet/Program.cs
+++ Shet/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Shet
 {
@@ -12,6 +13,14 @@ namespace Shet
         {
             // короче создаешь этот проект и проект модульного теста. Главное не забыть правой кнопкой мыши нажать в обозревателе решений на тестовый проект и добавить ссылку на основной проект
             // а дальше просто юзинг -- название основного проекта -- и инициализировать класс
+            TextWriterTraceListener[] listeners = new TextWriterTraceListener[] 
+            {
+            new TextWriterTraceListener("C:\\Users\\zelen\\Desktop\\Подготовка к экзамену\\Программы\\pop.txt"),
+            new TextWriterTraceListener(Console.Out)
+            };
+            Debug.Listeners.AddRange(listeners);
+            Debug.WriteLine("Some Value", "Some Category");
+            Debug.Flush();
         }
     }
     public class Plus
