fdcca2006dcce26ca896197b5d1473a14e2b0d8e
denzelcrocker <zelentsov36@gmail.com>
2022-07-03 12:26:28 +0300

    itog

--- Shet/Program.cs
+++ Shet/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.IO;
 
 namespace Shet
 {
@@ -21,11 +22,48 @@ namespace Shet
             Debug.Listeners.AddRange(listeners);
             Debug.WriteLine("Some Value", "Some Category");
             Debug.Flush();
+
         }
     }
     public class Plus
     { 
         public int sum(int a, int b)
         { return a + b; }
+        public static void ReadStudents()
+        {
+            StreamReader students = new StreamReader(@"C:\Users\zelen\Desktop\ElectronicGradeBook\ElectronicGradeBook\\students.txt");
+
+            string line = students.ReadLine();
+            string[] start = { "ФИО студента", "Год зачисления", "Форма обучения", "Специальность" };
+            string table = start[0];
+            int i = 0;
+
+            Console.WriteLine($"|{$"{"--"}",2}-|-{$"{"------------------------------------"}",35}----|-{$"----------------",15}----|-{$"----------------",15}----|-{$"---------------",15}-|");
+            Console.WriteLine($"|{$"{"№"}",2} | {$"{start[0]}",35}     | {$"{start[1]}",15}     | {$"{start[2]}",15}     | {$"{start[3]}",15} |");
+            Console.WriteLine($"|{$"{"--"}",2}-|-{$"{"------------------------------------"}",35}----|-{$"----------------",15}----|-{$"----------------",15}----|-{$"---------------",15}-|");
+
+            while (line != null)
+            {
+                i++;
+                string[] splitLine = line.Split(';');
+                string fio = splitLine[0];
+
+                Console.WriteLine($"|{$"{i}",2} | {$"{splitLine[0]}",35}     | {$"{splitLine[1]}",15}     | {$"{splitLine[2]}",15}     | {$"{splitLine[3]}",15} |");
+                Console.WriteLine($"|{$"{"--"}",2}-|-{$"{"------------------------------------"}",35}----|-{$"----------------",15}----|-{$"----------------",15}----|-{$"---------------",15}-|");
+
+                line = students.ReadLine();
+            }
+            students.Close();
+        }
+
+        public static void PlusStudents(string[] DataPlus)
+        {
+            StreamWriter students = new StreamWriter(@"C:\Users\zelen\Desktop\ElectronicGradeBook\ElectronicGradeBook\\students.txt", append: true);
+
+            string line = $"{DataPlus[0]};{DataPlus[1]};{DataPlus[2]};{DataPlus[3]}";
+
+            students.WriteLine(line);
+            students.Close();
+        }
     }
 }
